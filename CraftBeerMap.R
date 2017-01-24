c0cb0a009277d0a551575cad31212b6d6dfdeb53
David S. Jordan <David_S_Jordan@ncsu.edu>
2017-01-24 12:13:53 -0500

    Code Updates
    -Removed marker clustering because of a bug
    -Added in Yelp Ratings
    -Added in Brewery Links

--- CraftBeerMap.R
+++ CraftBeerMap.R
@@ -3,6 +3,22 @@ library(leaflet,rgdal)
 setwd("/Users/David_Jordan/Google Drive/Special Projects/CLTCraftBeer/CLTCraftBeer/")
 
 #Reading in Craft Beer Data
+Breweries <- read.csv("Beer Data.csv")
 
+#Getting the Frequency of Year Opened
+#Brew_Freq <- table(Breweries$Year.Opened)
+#Brew_Freq
 
-#Plotting Beer Map
+#Creating Icons for Map Markers
+beer_bottle <- makeIcon("empty-beer-bottle.png","empty-beer-bottle.png",36,36)
+
+#Plotting Beer Map Using Leaflet
+leaflet() %>%
+  addProviderTiles("OpenStreetMap") %>%
+  addMarkers(data=Breweries,
+             icon=beer_bottle, 
+             popup=paste("<b>","Brewery: ","</b>",paste("<a href=",Breweries$Brewery.Website,">",Breweries$Brewery,"</a>"), "<br>", "<b>","Neighborhood: ","</b>", Breweries$Neighborhood,"<br>","<b>","Avg. Yelp Score: ","</b>",Breweries$Average.Yelp.Rating),
+             clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F)
+             )
+
+    
