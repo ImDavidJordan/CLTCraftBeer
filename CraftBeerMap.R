a683814f54a790d68789c4e3d8379d30bb6065ab
David S. Jordan <David_S_Jordan@ncsu.edu>
2017-01-25 16:47:20 -0500

    Update 2 1-25-16
    -Got shiny integration working

--- CraftBeerMap.R
+++ CraftBeerMap.R
@@ -1,4 +1,7 @@
 library(leaflet,rgdal,data.table)
+library(shiny)
+
+
 
 setwd("/Users/David_Jordan/Google Drive/Special Projects/CLTCraftBeer/CLTCraftBeer/")
 
@@ -29,36 +32,94 @@ Breweries_New <- merge(Breweries, Beer_List3, by="Brewery")
 #Creating Icons for Map Markers
 beer_bottle <- makeIcon("empty-beer-bottle.png","empty-beer-bottle.png",36,36)
 
-#Plotting Beer Map Using Leaflet
-leaflet() %>%
-  addProviderTiles("OpenStreetMap") %>%
-  addMarkers(data=Breweries_New,
-             icon=beer_bottle, 
-             popup=paste(
-               "<b>",
-               "Brewery: ",
-               "</b>",
-               paste("<a href=",Breweries_New$Brewery.Website,
-                     ">",
-                     Breweries_New$Brewery,
-                     "</a>"), 
-               "<br>", 
-               "<b>",
-               "Neighborhood: ",
-               "</b>", 
-               Breweries_New$Neighborhood,
-               "<br>",
-               "<b>",
-               "Avg. Yelp Score: ",
-               "</b>",
-               Breweries_New$Average.Yelp.Rating,
-               "<br>",
-               "<b>",
-               "Styles: ",
-               "</b>",
-               Breweries_New$Styles
-               ),
-             clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F)
-             )
+ui <- navbarPage("Charlotte Craft Breweries Map", id="nav",
+                  tabPanel("Interactive Map",
+                           wellPanel(selectInput("Styles", 
+                                                 "Beer Styles",
+                                                 choices=unique(Beer_List$Style),
+                                                 multiple=FALSE)),
+                           leafletOutput("CharlotteCraftMap")
+                          
+                 ),
+                 tabPanel("Data Viewer",
+                          wellPanel(h3("Beer Statistics"), plotlyOutput("BreweryOpen")))
+                 )
 
+
+
+server <- function(input, output, session) {
+
+  output$CharlotteCraftMap <- renderLeaflet({
+  #Plotting Beer Map Using Leaflet
+  leaflet(Breweries_New) %>%
+      addProviderTiles("OpenStreetMap")%>%
+      addMarkers(icon=beer_bottle, 
+                 popup=paste(
+                   "<b>",
+                   "Brewery: ",
+                   "</b>",
+                   paste("<a href=",Breweries_New$Brewery.Website,
+                         ">",
+                         Breweries_New$Brewery,
+                         "</a>"), 
+                   "<br>", 
+                   "<b>",
+                   "Neighborhood: ",
+                   "</b>", 
+                   Breweries_New$Neighborhood,
+                   "<br>",
+                   "<b>",
+                   "Avg. Yelp Score: ",
+                   "</b>",
+                   Breweries_New$Average.Yelp.Rating,
+                   "<br>",
+                   "<b>",
+                   "Styles: ",
+                   "</b>",
+                   Breweries_New$Styles
+                 )
+      )
+  })
+  
+  filteredData <- reactive({
+    Filtered_Breweries <- subset(Breweries_New, grepl(input$Styles,Breweries_New$Styles))
+  })
+  observe({
+    leafletProxy("CharlotteCraftMap",data=filteredData()) %>%
+      clearMarkers() %>%
+      addMarkers(icon=beer_bottle, 
+                 popup=paste(
+                   "<b>",
+                   "Brewery: ",
+                   "</b>",
+                   paste("<a href=",filteredData()[,7],
+                         ">",
+                         filteredData()[,1],
+                         "</a>"), 
+                   "<br>", 
+                   "<b>",
+                   "Neighborhood: ",
+                   "</b>", 
+                   filteredData()[,5],
+                   "<br>",
+                   "<b>",
+                   "Avg. Yelp Score: ",
+                   "</b>",
+                   filteredData()[,6],
+                   "<br>",
+                   "<b>",
+                   "Styles: ",
+                   "</b>",
+                   filteredData()[,9]
+                 )
+      )
+  })
+  
+  #Creating plots from beer data
+  output$BreweryOpen<-renderPlotly({
+    Brewery_Openings<- count(Breweries_New$Year.Opened)
+    plot_ly(x = ~Brewery_Openings$x, y = ~Brewery_Openings$freq, mode="lines")
+    })
+}
     
+shinyApp(ui, server)
